70f7ad0d0c0f90f2f97ba582d2ef343f57c4d4a4
notmaineyy <shermaineang2802@gmail.com>
2022-10-20 20:40:59 +0100

    20 oct lab

--- task1/readme.md
+++ task1/readme.md
@@ -1,1 +1,6 @@
 Create your design in this folder.
+
+Waveform Description:
+2nd rising edge --> i=0
+rst = 1 when i < 2 (when 0,1,2)
+en = 1 when i > 4 (i==5)
---
+++ task2/counter_tb.cpp
@@ -0,0 +1,54 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env){
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc,argv);
+    // init top verilog instance
+    Vcounter* top = new Vcounter;
+    //init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp,99);
+    tfp->open("counter.vcd");
+
+    // init Vbuddy (open and initialise Vbuddy connection. port path is in vbuddy.cfg)
+    if (vbdOpen()!=1) return (-1);
+    vbdHeader("Lab 1: Counter");
+    //initialize simualation inputs
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+
+    //run simulation for many clock cycles
+    for (i=0; i<300;i++){
+
+        //dump variables into VCD file and toggle clock
+        for (clk=0;clk<2;clk++){
+            tfp->dump(2*i+clk);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+        // ++++ send count value to Vbuddy (output count values to 7-seg display every cycle)
+        vbdHex(4,(int(top->count)>>12)&0xF);
+        vbdHex(3,(int(top->count)>>8)&0xF);
+        vbdHex(2,(int(top->count)>>4)&0xF);
+        vbdHex(1,int(top->count) &0xF);
+        vbdCycle(i+1);
+        // ----- end of Vbuddy output section
+
+        // change input stimuli
+        top->rst = (i<2) | (i==15);
+        top->en = vbdFlag();
+        if (Verilated::gotFinish()) exit(0);
+    }
+
+    vbdClose(); //++++ housekeeping
+    tfp->close();
+    exit(0);
+}
